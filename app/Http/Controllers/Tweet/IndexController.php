7a4c7529d0936b933f287fdef3f038beab0d0e5d
RubyLrving <seimprogrammer@gmail.com>
2024-11-05 14:02:47 +0900

    サービスコンテナのコードを追加

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
-USe App\Models\Tweet;
+use App\Services\TweetService;
 use Illuminate\Http\Request;
 
 
@@ -12,10 +12,10 @@ class IndexController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
         //
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        $tweets = $tweetService->getTweets();
         return view('tweet.index')
             ->with('tweets', $tweets);
     }
