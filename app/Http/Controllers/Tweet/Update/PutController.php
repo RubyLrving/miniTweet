a24dd222abd9f953def569b20c89e1ec8859a906
RubyLrving <seimprogrammer@gmail.com>
2024-12-19 10:05:28 +0900

    loginを実装、user_idとtweetを紐づける

--- app/Http/Controllers/Tweet/Update/PutController.php
+++ app/Http/Controllers/Tweet/Update/PutController.php
@@ -6,16 +6,21 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Tweet\UpdateRequest;
 use App\Models\Tweet;
 use Illuminate\Http\Request;
+use App\Services\TweetService;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class PutController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(UpdateRequest $request)
+    public function __invoke(UpdateRequest $request, TweetService $tweetService)
     {
         //
         $tweet = Tweet::where('id', $request->id())->firstOrFail();
+        if (!$tweetService->checkOwnTweet($request->user()->id, $tweetId)){
+            throw new AccessDeniedHttpException();
+        }
         $tweet->content = $request->tweet();
         $tweet->save();
         return redirect()->route('tweet.update.index', ['tweetId' => $tweet->id])->with('feedback.success', "つぶやきを編集しました");
